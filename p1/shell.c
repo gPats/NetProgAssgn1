1e1f949eaac37f39ad89c19072019f4ef0adf9cc
Parth Kalgaonkar <parthkalgaonkar@gmail.com>
2019-02-06 21:37:56 +0530

    simple token parsing done

--- p1/shell.c
+++ p1/shell.c
@@ -1,5 +1,9 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+
+#define MAX_CMD_LEN 1000
+#define MAX_TOKENS 50
 
 char ** get_tokens(char *str);
 int check_valid(char ** tokens);
@@ -7,20 +11,36 @@ void put_tokens(char ** tokens);
 
 int main(){
 	char ** cmd;
-	char str[100]={};
-	while (1){
+	char str[MAX_CMD_LEN]={};
+	//while (1){
 		printf("$");
 		scanf("%[^\n]s", str);
+		str[strlen(str)]=0;
+		//printf("%s\n", str);
 		cmd=get_tokens(str);
 		put_tokens(cmd);
-	}
+		free(cmd);
+	//}
 }
 
 char ** get_tokens(char *str){
-	char **ret
+	char **ret=(char **) calloc(MAX_TOKENS, sizeof(char*));
 	char * tok;
 	tok=strtok(str," ");
+	int i=0;
 	while (tok!=NULL){
+		ret[i]=(char *) calloc(strlen(tok)+1, sizeof(char));
+		if(tok[0]!=0) strcpy(ret[i], tok);
+		tok=strtok(NULL," ");
+		i++;
+	}
+	return ret;
+}
 
+void put_tokens(char ** tokens){
+	int i=0;
+	while (tokens[i]!=NULL){
+		printf("%s\n", tokens[i]);
+		i++;
 	}
 }
