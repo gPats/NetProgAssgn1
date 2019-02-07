f21389bb717570fd702c37e233fb90e6e44db2dd
gPats <f2016134@pilani.bits-pilani.ac.in>
2019-02-08 03:17:17 +0530

    completed check_validity for one command

--- p1/shell.c
+++ p1/shell.c
@@ -1,27 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 #define MAX_CMD_LEN 1000
 #define MAX_TOKENS 50
-
-char ** get_tokens(char *str);
-int check_valid(char ** tokens);
-void put_tokens(char ** tokens);
-
-int main(){
-	char ** cmd;
-	char str[MAX_CMD_LEN]={};
-	//while (1){
-		printf("$");
-		scanf("%[^\n]s", str);
-		str[strlen(str)]=0;
-		//printf("%s\n", str);
-		cmd=get_tokens(str);
-		put_tokens(cmd);
-		free(cmd);
-	//}
-}
 
 char ** get_tokens(char *str){
 	char **ret=(char **) calloc(MAX_TOKENS, sizeof(char*));
@@ -43,4 +26,53 @@ void put_tokens(char ** tokens){
 		printf("%s\n", tokens[i]);
 		i++;
 	}
+}
+
+int check_valid(char** tokens) {
+	if (strchr(tokens[0],'/') != NULL)
+	{
+		if(access(tokens[0],F_OK) == 0)return 1;
+	}
+		char* search_path = getenv("PATH");
+
+		if(!search_path)return -1;
+		
+		char* buf = (char*)malloc(strlen(tokens[0]) + strlen(search_path) + 3);
+		
+		while (*search_path)
+		{
+			char* iter = buf;
+			for (;*search_path && *search_path != ':'; search_path++,iter++)
+			{
+				*iter = *search_path;
+			}
+			if(iter == buf)*iter++ = '.';
+			if(iter[-1] != '/')*iter++ = '/';
+			strcpy(iter,tokens[0]);
+			if(access(buf,F_OK) == 0) {
+				free(buf);
+				return 1;
+			}
+			if(!*search_path)break;
+			search_path++; 
+		}
+free(buf);
+return -1;
+}
+
+int main(){
+	char ** cmd;
+	char str[
+	MAX_CMD_LEN]={};
+	//while (1){
+		printf("$");
+		scanf("%[^\n]s", str);
+		str[strlen(str)]=0;
+		//printf("%s\n", str);
+		cmd=get_tokens(str);
+		put_tokens(cmd);
+		int ret = check_valid(cmd);
+		printf("%d\n",ret);
+		free(cmd);
+	//}
 }
